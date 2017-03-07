3b9340b32f02185b14c5b872935ade3fef17369c
MykeMcG <myke.mcg@gmail.com>
2017-03-07 12:49:41 -0400

    Add BitmapFile
    Added BitmapFile and backend project, pulling some of the code out of
    the front end.

---
+++ PaletteExtractor.Backend/BitmapFile.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PaletteExtractor.Backend
+{
+  public class BitmapFile
+  {
+    public string Path { get; private set; }
+
+    public BitmapFile(string path)
+    {
+      EnsurePathLeadsToImage(path);
+      EnsureFileExists(path);
+      this.Path = path;
+    }
+
+    public Bitmap ExtractPalette(int imageWidth)
+    {
+      var bitmap = new Bitmap(this.Path);
+      List<Color> colorList = this.GetColorList(bitmap);
+      Bitmap output = this.GenerateImage(colorList, imageWidth);
+      return output;
+    }
+
+    private void EnsurePathLeadsToImage(string path)
+    {
+      string extension = System.IO.Path.GetExtension(path);
+      switch (extension)
+      {
+        case ".png":
+        case ".bmp":
+        case ".gif":
+        case ".jpg":
+        case ".jpeg":
+          return;
+        default:
+          throw new ArgumentException("Invalid file path provided. File must be an image.");
+      }
+    }
+
+    private void EnsureFileExists(string path)
+    {
+      bool exists = File.Exists(path);
+      if (!exists)
+      {
+        throw new ArgumentException("Specified file does not exist.");
+      }
+    }
+
+    private List<Color> GetColorList(Bitmap bitmapToProcess)
+    {
+      List<Color> colors   = new List<System.Drawing.Color>();
+      for (int y = 0; y < bitmapToProcess.Height; y++)
+      {
+        for (int x = 0; x < bitmapToProcess.Width; x++)
+        {
+          Color currentPixel = bitmapToProcess.GetPixel(x, y);
+          Int32 colorMatchCount = (from c in colors
+                                   where c.A == currentPixel.A
+                                             &&    c.R == currentPixel.R
+                                             &&    c.G == currentPixel.G
+                                             &&    c.B == currentPixel.B
+                                   select c).Count();
+          if (colorMatchCount == 0)
+          {
+            colors.Add(currentPixel);
+          }
+        }
+      }
+      return colors;
+    }
+
+    private Bitmap GenerateImage(List<Color> colorList, int imageWidth)
+    {
+      Int32 imageHeight     = Convert.ToInt32(Math.Ceiling((Double)colorList.Count / imageWidth));
+      Int32 colorIndex      = 0;
+      Bitmap generatedImage = new Bitmap(imageWidth, imageHeight);
+      for (int y = 0; y < imageHeight; y++)
+      {
+        for (int x = 0; x < imageWidth; x++)
+        {
+          if (colorIndex <= colorList.Count() - 1)
+          {
+            generatedImage.SetPixel(x, y, colorList[colorIndex]);
+            colorIndex++;
+          }
+        }
+      }
+      return generatedImage;
+    }
+  }
+}
--- PaletteExtractor/MainForm.cs
+++ PaletteExtractor/MainForm.cs
@@ -7,114 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PaletteExtractor.Backend;
 
 namespace PaletteExtractor
 {
-    public partial class MainForm : Form
+  public partial class MainForm : Form
+  {
+    public MainForm()
     {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
-
-        private List<Color> GetColorList(Bitmap imageToProcess)
-        {
-            List<Color> colors   = new List<System.Drawing.Color>();
-            progressBar1.Maximum = imageToProcess.Height;
-            progressBar1.Value   = 0;
-            for (int y = 0; y < imageToProcess.Height; y++)
-            {
-                for (int x = 0; x < imageToProcess.Width; x++)
-                {
-                    Color currentPixel = imageToProcess.GetPixel(x, y);
-                    Int32 colorMatchCount = (from c in colors
-                                             where c.A == currentPixel.A
-                                             &&    c.R == currentPixel.R
-                                             &&    c.G == currentPixel.G
-                                             &&    c.B == currentPixel.B
-                                             select c).Count();
-                    if (colorMatchCount == 0)
-                    {
-                        colors.Add(currentPixel);
-                    }
-                }
-                progressBar1.Step = 1;
-                progressBar1.PerformStep();
-            }
-            return colors;
-        }
-
-        private Bitmap GenerateColorBitmap(List<Color> colorList)
-        {
-            Int32 imageWidth      = Properties.Settings.Default.OutputWidth;
-            Int32 imageHeight     = Convert.ToInt32(Math.Ceiling((Double)colorList.Count / imageWidth));
-            Int32 colorIndex      = 0;
-            Bitmap generatedImage = new Bitmap(imageWidth, imageHeight);
-            for (int y            = 0; y < imageHeight; y++)
-            {
-
-                for (int x = 0; x < imageWidth; x++)
-                {
-                    if (colorIndex <= colorList.Count() - 1)
-                    {
-                        generatedImage.SetPixel(x, y, colorList[colorIndex]);
-                        colorIndex++;
-                    }
-                }
-            }
-            return generatedImage;
-        }
-
-        #region "Event Handlers"
-
-        private void btnInput_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.Filter   = "Image files|*.png;*.jpg;*.jpeg;*.bmp;*.gif";
-            openFileDialog1.FileName = string.Empty;
-            openFileDialog1.ShowDialog();
-            txtInput.Text = openFileDialog1.FileName;
-        }
-
-        private void btnGo_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                String inputPath         = txtInput.Text;
-                Bitmap inputImage        = new Bitmap(inputPath);
-                this.Text                = "Palette Extractor [Getting Colors...]";
-                List<Color> colors       = GetColorList(inputImage);
-                this.Text                = "Palette Extractor [Generating Image...]";
-                Bitmap outputImage       = GenerateColorBitmap(colors);
-                saveFileDialog1.Filter   = "Image files|*.png;*.jpg;*.jpeg;*.bmp;*.gif";
-                saveFileDialog1.FileName = "Output.png";
-                saveFileDialog1.ShowDialog();
-                String outputPath = saveFileDialog1.FileName;
-                outputImage.Save(outputPath);
-                this.Text = "Palette Extractor";
-                MessageBox.Show("Done!");
-            }
-            catch (ArgumentException)
-            {
-                MessageBox.Show("File not found");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Options dialog = new Options();
-            dialog.StartPosition = FormStartPosition.CenterParent;
-            dialog.ShowDialog();
-        }
-
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        #endregion// Event Handlers
+      InitializeComponent();
     }
+
+    #region Event Handlers
+
+    private void btnInput_Click(object sender, EventArgs e)
+    {
+      openFileDialog1.Filter = "Image files|*.png;*.jpg;*.jpeg;*.bmp;*.gif";
+      openFileDialog1.FileName = string.Empty;
+      openFileDialog1.ShowDialog();
+      txtInput.Text = openFileDialog1.FileName;
+      BitmapFile x = new BitmapFile(openFileDialog1.FileName);
+    }
+
+    private async void btnGo_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        String inputPath         = txtInput.Text;
+        BitmapFile file = new BitmapFile(inputPath);
+        Bitmap outputImage       = file.ExtractPalette(Properties.Settings.Default.OutputWidth);
+        saveFileDialog1.Filter = "Image files|*.png;*.jpg;*.jpeg;*.bmp;*.gif";
+        saveFileDialog1.FileName = "Output.png";
+        saveFileDialog1.ShowDialog();
+        String outputPath = saveFileDialog1.FileName;
+        outputImage.Save(outputPath);
+        this.Text = "Palette Extractor";
+        MessageBox.Show("Done!");
+      } catch (ArgumentException)
+      {
+        MessageBox.Show("File not found");
+      } catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+    }
+
+    private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Options dialog = new Options();
+      dialog.StartPosition = FormStartPosition.CenterParent;
+      dialog.ShowDialog();
+    }
+
+    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+    #endregion Event Handlers
+  }
 }
